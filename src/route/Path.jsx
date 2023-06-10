d835225d23ebfbcba66e832f0501ae1cb3aeb626
khinezinthin <khinezinthinwk1@gmail.com>
2023-06-11 00:28:37 +0630

    sankyitar

--- src/route/Path.jsx
+++ src/route/Path.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "../components/Footer";
 import Home from "../pages/Home";
+import ContactUs from "../pages/ContactUs";
 
 const Path = () => {
   return (
@@ -9,6 +10,7 @@ const Path = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/contactUs" element={<ContactUs />} />
         </Routes>
         <Footer/>
       </BrowserRouter>
