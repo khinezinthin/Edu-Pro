b00127b7ffef8ffb66e65ebe9c4eae0191631db5
khinezinthin <khinezinthinwk1@gmail.com>
2023-06-11 21:22:17 +0630

    finish register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -14,28 +14,24 @@ const Login = () => {
                 </div>
 
                 <div className=''>
-                    <form className=' w-[330px] max-[300px]:w-[300px] border border-gray-400 p-7 bg-[#fff]'>
-                        <TextInput label="Username or Email Address"  />
+                    <form className=' w-[360px] max-[400px]:w-[340px] max-[370px]:w-[300px] max-[320px]:w-[280px] border border-gray-400 p-7 bg-[#fff]'>
+                        <TextInput label="Username"  />
+                        <br />
+                        <TextInput label="Email Address"  />
                         <PasswordInput mt="md" label="Password" />
                         <br />
-                        <div className=' flex justify-between'>
-                            <Checkbox label="Remember Me"/>
-                            <button className=' py-2 px-3 bg-[#2271b1] rounded text-white text-sm'>
-                                Log In
+                            <button className=' py-2 px-5 bg-[#2271b1] rounded text-white text-sm'>
+                                Register
                             </button>
-                        </div>
-                        <p className=' mt-2 text-sm text-gray-600 '>Not Register ?
-                         <Link to='/register'>
+                        <p className=' mt-5 text-sm text-gray-600 '>Already have an account ?
+                         <Link to='/login'>
                             <span className=' cursor-pointer hover:text-[#6db1e9] hover:border-b-[#6db1e9] text-[#2271b1] ml-2 border-b border-b-blue-500'>
-                                Sign Up
+                                Sign In
                             </span>
                          </Link>
                         </p>
                     </form>
                     <div className=' mt-5 flex flex-col gap-2 pl-5'>
-                        <p className=' text-sm text-gray-600 hover:text-[#2271b1] cursor-pointer'>
-                            Lost your password?
-                        </p>
                         <Link to='/'>
                             <div className=' flex items-center gap-1 text-sm text-gray-600 hover:text-[#2271b1] cursor-pointer'>
                                 <BsArrowLeft/>
