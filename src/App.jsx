03ec9999116350b638363aaf834b1d2937966f88
thaepannphyu <phyu24655@gmail.com>
2023-06-11 23:59:34 +0630

    BlogStandard-1st

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,18 @@ import Navbar from "./components/Navbar.jsx";
 import "./App.css";
 import FreeWorkshop from "./components/AboutUsPagesComponent/FreeWorkshop";
 import WhyChooseUs from "./components/HomeComponents/WhyChooseUs";
-
+import Testimonial from "./components/HomeComponents/Testimonial";
+import BlogStandardRight from "./components/BlogStandardComponents/BlogStandardRight";
 const App = () => {
   return (
     <div className=" relative">
-      {/* <Navbar /
+      {/* <Navbar />
 
-      <Path /> */}
+      <Path />
       <FreeWorkshop />
       <WhyChooseUs />
+      <Testimonial /> */}
+      {/* <BlogStandardRight /> */}
     </div>
   );
 };
