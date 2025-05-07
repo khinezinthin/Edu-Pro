8ab75ce51ad7c87ee359c7f7358d6d5ed88477f0
khinezinthin <khinezinthinwk1@gmail.com>
2025-05-07 11:30:56 +0630

    remove freeworkshop

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Knowledge from "../components/AboutUsPagesComponent/Knowledge"
 import WhatweOffer from "../components/AboutUsPagesComponent/WhatweOffer"
-import FreeWorkShop from "../components/AboutUsPagesComponent/FreeWorkShop"
+// import FreeWorkShop from "../components/AboutUsPagesComponent/FreeWorkShop"
 import Learners from '../components/AboutUsPagesComponent/Learners'
 import Skilled from "../components/AboutUsPagesComponent/Skilled"
 import Feedback from "../components/AboutUsPagesComponent/Feedback"
